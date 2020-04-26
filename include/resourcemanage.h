f43ac4eca3cb44e8f3641b6a6ad3f13ca32f508b
baoguo.chen <baoguo.chen@amlogic.com>
2020-04-26 19:52:27 +0800

    tsplayer: tsplayer port tvp manage [3/3]
    PD#SWPL-24073
    
    Problem:
    tsplayer port resmanage to enable/disable tvp.
    
    Solution:
    tsplayer port resmanage to enable/disable tvp.
    
    Verify:
    newton
    
    SourceInfo:
    26877513280f90a11af7715627582929d9ef8045 +
    I08ed881e7971cc16144430f0ef41f955cfc3fa1e
    
    Change-Id: Ia5729bb0cb42fc3f4cc569372d1b85b88afcb1e6
    Signed-off-by: baoguo.chen <baoguo.chen@amlogic.com>

--- include/resourcemanage.h
+++ include/resourcemanage.h
@@ -18,12 +18,14 @@ extern "C" {
 #define    RESMAN_IOC_ACQUIRE_RES        _IOW(RESMAN_IOC_MAGIC, 0x02, int)
 #define    RESMAN_IOC_RELEASE_RES        _IOR(RESMAN_IOC_MAGIC, 0x03, int)
 #define    RESMAN_IOC_SETAPPINFO         _IOW(RESMAN_IOC_MAGIC, 0x04, int)
+#define    RESMAN_IOC_SUPPORT_RES        _IOR(RESMAN_IOC_MAGIC, 0x05, int)
 
 #define BASE_AVAILAB_RES               (0x0)
 #define VFM_DEFAULT                    (BASE_AVAILAB_RES + 0)
 #define AMVIDEO                        (BASE_AVAILAB_RES + 1)
 #define PIPVIDEO                       (BASE_AVAILAB_RES + 2)
-#define TSPARSER                       (BASE_AVAILAB_RES + 3)
+#define SEC_TVP                        (BASE_AVAILAB_RES + 3)
+#define TSPARSER                       (BASE_AVAILAB_RES + 4)
 #define MAX_AVAILAB_RES                (TSPARSER + 1)
 
 struct resman_para {
@@ -41,6 +43,7 @@ typedef enum {
     TYPE_OMX      = 0,
     TYPE_DVB,
     TYPE_HDMI_IN,
+    TYPE_SEC_TVP,
     TYPE_OTHER    = 10,
 }APP_TYPE;
 
@@ -52,6 +55,7 @@ int resman_acquire(int handle, int restype);
 int resman_release(int handle, int restype);
 int resman_query(int handle, struct resman_para *res_status);
 bool resman_acquire_wait(int handle, int restype, const unsigned int time_out);//Timeout unit: milliseconds
+bool resman_resource_support(const char* resname);
 
 #ifdef  __cplusplus
 }
