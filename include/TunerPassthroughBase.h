e789d8b3d53db20f715791352c7d208e968d8fc5
Kai Song <kai.song@amlogic.com>
2021-07-19 20:25:14 +0800

    videopassthrough: CF2 Support get video decoder buffer status [3/3]
    PD#SWPL-54846
    
    Problem:
    Play local files incompletely with tuner framework passthrough mode
    There is no video flow control for passthrough mode, we need feedback
    the decoder buffer level to the apk layer to control dvr data write speed.
    
    Solution:
    Support get video decoder buffer status and callback to omx component.
    
    Verify:
    Android-R & OHM
    SourceCodec:
    V3.2.19-I30933-7932fe
    7932fe0 videopassthrough: CF2 Support get video decoder buffer status [3/3]
    a54fed2 mediahal_sdk: CB0 Android Q failed to compile the firmware [1/1]
    e0054d3 MediaSync: CF1 av is not sync when switch channel in multi-channel play.
    1202a90 TsPlayer: CB1 TsPlayer cann't release resource if it create failed [1/1]
    499638e MediaSync: CF1 FCC switch channel, the speed is slow. [1/1]
    785ff14 TsPlayer: CB2 open PIP,press "Swap", amvideodec crash [1/1]
    8735871 tsplayer: CF1 set setParams [1/1]
    d0b542b tsplayer: CF1 android P T3 chip, DTV use video_tunnel to display [1/1]
    5352e15 tsplayer: CB1 audio and video are out of sync after seek. [1/1]
    92773aa MediaSync: CB1 Micro speed no effect issue [1/1]
    
    Signed-off-by: Kai Song <kai.song@amlogic.com>
    Change-Id: I2798dc31d8034ae20a1bfedd4198b50f6ec8c335

--- include/TunerPassthroughBase.h
+++ include/TunerPassthroughBase.h
@@ -11,14 +11,26 @@
 
 #include <stdint.h>
 
+typedef int (*callbackFunc)(void*obj, void* args);
+
+typedef struct DECODERBUFFERSTATUS {
+    int64_t nFreePercentage;
+    int64_t nSystemTimeNs;
+} DECODERBUFSTATUS;
+
 struct TunerPassthroughBase
 {
 
 public:
+    enum {
+        CB_NODIFYDECODERBUFFERSTATUS,
+        CB_FUNS_MAX,
+    };
 
     TunerPassthroughBase() {};
     virtual ~TunerPassthroughBase() {};
     virtual int Init(int dmx_id, int video_pid, const char* mime, int hw_sync_id, int secure_mode);
+    virtual int RegCallBack(int cb_id, callbackFunc funs, void* obj);
     virtual int GetSyncInstansNo(int *no);
     virtual int Start();
     virtual int Stop();
