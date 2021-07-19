a8d5836aaab9d66a7c4bcd5ac172a659e7bd3f27
Kai Song <kai.song@amlogic.com>
2021-07-19 20:25:14 +0800

    videopassthrough: CF2 Support get video decoder buffer status [3/3]
    PD#SWPL-54846
    
    Problem:
    Play local files incompletely with tuner framework passthrough mode
    There is no video flow control for passthrough mode, we need feedback
    the decoder buffer level to the apk layer to control dvr data write speed.
    
    Solution:
    Support get video decoder buffer status and callback to omx component.
    
    Verify:
    Android-R & OHM
    SourceCodec:
    V3.2.19-I30933-7932fe
    7932fe0 videopassthrough: CF2 Support get video decoder buffer status [3/3]
    a54fed2 mediahal_sdk: CB0 Android Q failed to compile the firmware [1/1]
    e0054d3 MediaSync: CF1 av is not sync when switch channel in multi-channel play.
    1202a90 TsPlayer: CB1 TsPlayer cann't release resource if it create failed [1/1]
    499638e MediaSync: CF1 FCC switch channel, the speed is slow. [1/1]
    785ff14 TsPlayer: CB2 open PIP,press "Swap", amvideodec crash [1/1]
    8735871 tsplayer: CF1 set setParams [1/1]
    d0b542b tsplayer: CF1 android P T3 chip, DTV use video_tunnel to display [1/1]
    5352e15 tsplayer: CB1 audio and video are out of sync after seek. [1/1]
    92773aa MediaSync: CB1 Micro speed no effect issue [1/1]
    
    Signed-off-by: Kai Song <kai.song@amlogic.com>
    Change-Id: I2798dc31d8034ae20a1bfedd4198b50f6ec8c335

--- mediahalvercheck.go
+++ mediahalvercheck.go
@@ -14,7 +14,7 @@ func MediahalVerCheckDefaults(ctx android.LoadHookContext) {
     if err != nil {
         fmt.Printf("%v fail to convert", sdkVersionInt)
     } else {
-        fmt.Println("sdkVersion:", sdkVersionInt)
+        fmt.Println("CheckDefaults sdkVersion:", sdkVersionInt)
     }
     if sdkVersionInt >= 30 {
         type props struct {
@@ -26,13 +26,39 @@ func MediahalVerCheckDefaults(ctx android.LoadHookContext) {
     }
 }
 
+func MediahalVerCheckMediahalPasshroughDefaults(ctx android.LoadHookContext) {
+    sdkVersion := ctx.DeviceConfig().PlatformVndkVersion()
+    sdkVersionInt,err := strconv.Atoi(sdkVersion)
+    if err != nil {
+        fmt.Printf("%v fail to convert", sdkVersionInt)
+    } else {
+        fmt.Println("PasshroughDefaults sdkVersion:", sdkVersionInt)
+    }
+    if sdkVersionInt < 30 {
+        type props struct {
+            Enabled  *bool
+        }
+        p := &props{}
+        p.Enabled = proptools.BoolPtr(false)
+        ctx.AppendProperties(p)
+    }
+}
+
 func init() {
     android.RegisterModuleType("mediahalvercheck_defaults", MediahalVerCheckDefaultsFactory)
+    android.RegisterModuleType("mediahalvercheck_mediahal_passthrough_defaults", MediahalVerCheckMediahalPasshroughDefaultsFactory)
 }
 
 func MediahalVerCheckDefaultsFactory() android.Module {
     module := cc.DefaultsFactory()
     android.AddLoadHook(module, MediahalVerCheckDefaults)
+
+    return module
+}
+
+func MediahalVerCheckMediahalPasshroughDefaultsFactory() android.Module {
+    module := cc.DefaultsFactory()
+    android.AddLoadHook(module, MediahalVerCheckMediahalPasshroughDefaults)
 
     return module
 }
