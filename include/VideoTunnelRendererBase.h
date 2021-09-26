7ec716d6d12f1599f306e5422d49644926dfc76b
Peter Wang <peter.wang@amlogic.com>
2021-09-26 21:10:55 +0800

    MediaHal: CF1 implement android S video peek [1/2]
    PD#SWPL-48015
    
    Problem:
    android S add video peek function and cts test cases bellow
    android.media.cts.DecoderTest#testTunneledVideoPeekDefault
    android.media.cts.DecoderTest#testTunneledVideoPeekOff
    
    Solution:
    implement Android S video peek function
    
    Verify:
    test on ohm
    
    Build Tag:
    Android_R_DRM_PATCHBUILD-1913
    
    Commits:
    media_hal:
    version: 3.2.86-g693d6c8(build:1913)
    693d6c8 MediaHal: CF1 implement android S video peek [1/2]
    
    Git Hash-Object:
    0c62a7fe9d0491514ba6598281cdd2699a0e0caf
     include/VideoTunnelRendererBase.h
    67ad78ac8dee05eb7b1e6b29c211020c9f2c4414
     lib/system/libmediahal_tsplayer.system.so
    8b31d8cdd29289a48fb603e581f13b0d11f85b81
     lib/system/libmediahal_videodec.system.so
    97cb2cbd5a1116ffd63cb4cab181dab923039180
     lib/vendor/libmediahal_passthrough.so
    1cdf23ef07b8d9b3cfbf8e8f593501ace709f524
     lib/vendor/libmediahal_tsplayer.so
    404998488dfdfa2e32e6e2cea36720ee7b8550b1
     lib/vendor/libmediahal_tunnelrenderer.so
    1069733d515b521c77c432cac10276c2326a5a72
     lib/vendor/libmediahal_videodec.so
    e4ee98b500ca84a4b37502b4f214c090e0913ee2
     version.txt
    
    Change-Id: I4e97b131f648e55656bb6fb1d7eee079c1052801
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- include/VideoTunnelRendererBase.h
+++ include/VideoTunnelRendererBase.h
@@ -40,6 +40,7 @@ public:
     virtual int regCallBack(int cb_id, callbackFunc funs, void* obj);
     virtual bool flush();
     virtual bool setFrameRate(int32_t framerate);
+    virtual bool peekFirstFrame();
     virtual void onVideoSyncQueueVideoFrame(int64_t timestampUs);
 };
 
